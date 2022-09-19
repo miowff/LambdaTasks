aec4eb19d93e6c4c678cde4ec61e45f5b2d21562
Ocheretniuk nikolay <chkalovets55@gmail.com>
2022-09-19 13:27:08 +0300

    vacation group compleated

---
+++ Vacation group/Models/UserModel.js
@@ -0,0 +1,9 @@
+export class UserModel
+{
+    constructor(userId,name,weekendDates)
+    {
+        this.userId = userId;
+        this.name = name;
+        this.weekendDates = weekendDates;
+    }
+}

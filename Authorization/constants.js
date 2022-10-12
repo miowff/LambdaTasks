7e8547c4102c5db26b9000071be6f1141213b37f
Ocheretniuk nikolay <chkalovets55@gmail.com>
2022-10-12 11:46:25 +0300

    monodbUrl hided

--- Authorization/constants.js
+++ Authorization/constants.js
@@ -3,4 +3,3 @@ export const PORT = process.env.PORT ?? 3000;
 export const SECRET = "DPwZqIgFrwDebghywePPa";
 export const RFRESH_SECRET = "ZDdfsdjfHJJFDgg";
 
-export const MONGO_DB_CONNECTION_URL = "mongodb+srv://Mykola:JqhV9UIAXppMSA6u@cluster0.8vibn79.mongodb.net/?retryWrites=true&w=majority";

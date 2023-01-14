1a742ef4c2d9df3e40503c0e8d812568030247fd
Mykola <95630245+miowff@users.noreply.github.com>
2023-01-14 09:39:23 +0200

    Update constants.js

--- CLI Interactive sort/constants.js
+++ CLI Interactive sort/constants.js
@@ -1,58 +1,27 @@
-<<<<<<< HEAD
-    const commands = {
-    Exit: '1',
-    AlphabetSort: '2',
-    SortNumbersByAscending: '3',
-    SortNumbersByDescanding:'4',
-    WordsByQuantityOfLetters:'5',
-    OnlyUniqueWords:'6',
-    OnlyUniqueValues:'7'
-  };
-
-  const ENTER_REQUEST = "Enter your words or dights: ";
-
-  const ENTER_COMMAND_PROMPT = `
-  Alphabet sort: Enter '2' 
-  Sort numbers by ascending: Enter '3' 
-  Sort numbers by descanding: Enter '4' 
-  Order words by quantity of letters: Enter '5' 
-  Only unique words: Enter '6'
-  Only unique values: Enter '7'
-  Exit: Enter '1'
-  `
-
-  module.exports = {
+const commands = 
+{
+Exit: '1',
+AlphabetSort: '2',
+SortNumbersByAscending: '3',
+SortNumbersByDescanding:'4',
+WordsByQuantityOfLetters:'5',
+OnlyUniqueWords:'6',
+OnlyUniqueValues:'7'
+};
+const ENTER_REQUEST = "Enter your words or dights: ";
+const ENTER_COMMAND_PROMPT = `
+Alphabet sort: Enter '2' 
+Sort numbers by ascending: Enter '3' 
+Sort numbers by descanding: Enter '4' 
+Order words by quantity of letters: Enter '5' 
+Only unique words: Enter '6'
+Only unique values: Enter '7'
+Exit: Enter '1'`
+module.exports = 
+{
     commands:commands,
     ENTER_REQUEST:ENTER_REQUEST,
     ENTER_COMMAND_PROMPT:ENTER_COMMAND_PROMPT
-  };
-=======
-    const commands = {
-    Exit: '1',
-    AlphabetSort: '2',
-    SortNumbersByAscending: '3',
-    SortNumbersByDescanding:'4',
-    WordsByQuantityOfLetters:'5',
-    OnlyUniqueWords:'6',
-    OnlyUniqueValues:'7'
-  };
+};
 
-  const ENTER_REQUEST = "Enter your words or dights: ";
-
-  const ENTER_COMMAND_PROMPT = `
-  Alphabet sort: Enter '2' 
-  Sort numbers by ascending: Enter '3' 
-  Sort numbers by descanding: Enter '4' 
-  Order words by quantity of letters: Enter '5' 
-  Only unique words: Enter '6'
-  Only unique values: Enter '7'
-  Exit: Enter '1'
-  `
-
-  module.exports = {
-    commands:commands,
-    ENTER_REQUEST:ENTER_REQUEST,
-    ENTER_COMMAND_PROMPT:ENTER_COMMAND_PROMPT
-  };
->>>>>>> db676f958f704a78e63aa455fc4c740ba3a3b69b
   

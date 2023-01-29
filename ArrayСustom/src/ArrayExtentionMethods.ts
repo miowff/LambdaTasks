f59a75f5a9f049c95f7bd1f5afc5688b7ceec2de
Ocheretniuk nikolay <chkalovets55@gmail.com>
2023-01-29 10:19:44 +0200

    array custom fixes

--- ArrayСustom/src/ArrayExtentionMethods.ts
+++ ArrayСustom/src/ArrayExtentionMethods.ts
@@ -1,234 +1,190 @@
 export {};
 
-Array.prototype.any = function <T>(predicate?: (value: T) => boolean) 
-{
-    if(predicate === undefined)
-    {
-        if(this.length>0)
-        {
-            return true;
-        }
-        return false;
+Array.prototype.any = function <T>(predicate?: (value: T) => boolean) {
+  if (predicate === undefined) {
+    if (this.length > 0) {
+      return true;
     }
-    return this.some(predicate);
+    return false;
+  }
+  return this.some(predicate);
 };
 
-Array.prototype.all = function<T>(predicate?:(value:T)=>boolean)
-{
-    if(predicate === undefined)
-    {
-        return false;
-    }
-    return this.every(predicate);
-}
+Array.prototype.all = function <T>(predicate?: (value: T) => boolean) {
+  if (predicate === undefined) {
+    return false;
+  }
+  return this.every(predicate);
+};
 
-Array.prototype.Find = function<T>(predicate:(value:T)=>boolean)
-{
-    for(let i = 0;i<this.length;i++)
-    {
-        if(predicate(this[i]))
-        {
-            return this[i];
-        }
+Array.prototype.Find = function <T>(predicate: (value: T) => boolean) {
+  this.forEach((element) => {
+    if (predicate(element)) {
+      return element;
     }
-    return;
-}
+  });
+  return;
+};
 
-Array.prototype.findLast = function<T>(predicate:(value:T)=>boolean)
-{
-    let result;
-    for(let i = 0;i<this.length;i++)
-    {
-        if(predicate(this[i]))
-        {
-            result = this[i];
-        }
+Array.prototype.findLast = function <T>(predicate: (value: T) => boolean) {
+  let result;
+  this.forEach((element) => {
+    if (predicate(element)) {
+      result = element;
+    }
+  });
+  return result;
+};
+
+Array.prototype.chunked = function (chunkSize: number) {
+  let chunksCount = this.length / chunkSize;
+  if (this.length % chunkSize != 0) {
+    chunksCount = Math.floor(chunksCount) + 1;
+  }
+  const result = [];
+  for (let i = 0; i < this.length; i += chunkSize) {
+    const chunk = this.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+  return result;
+};
+
+Array.prototype.average = function () {
+  let sum = 0;
+  this.forEach((element) => {
+    sum += element;
+  });
+  return sum / this.length;
+};
+
+Array.prototype.filterIndexed = function <T>(
+  predicate: (index: number, value: T) => boolean
+) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (predicate(i, this[i])) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+Array.prototype.filterNot = function <T>(predicate: (value: T) => boolean) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (!predicate(this[i])) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+Array.prototype.maxBy = function <T>(selector: (type: T) => number) {
+  let max = Number.MIN_VALUE;
+  let result = null;
+  this.forEach((element) => {
+    if (selector(element) > max) {
+      max = selector(element);
+      result = element;
+    }
+  });
+  return result;
+};
+
+Array.prototype.minBy = function <T>(selector: (type: T) => number) {
+  let min = Number.MAX_VALUE;
+  let result = null;
+  this.forEach((element) => {
+    if (selector(element) < min) {
+      min = selector(element);
+      result = element;
+    }
+  });
+  return result;
+};
+
+Array.prototype.fold = function (
+  initialValue: string | number,
+  operation: (value: string | number) => string | number
+) {
+  let result: string | number = initialValue;
+  this.forEach((element) => {
+    result += operation(element) as string;
+  });
+  return result;
+};
+
+Array.prototype.associateBy = function <K, V>(
+  keySelector: (key: K) => V
+): Map<K, V> {
+  const result = new Map();
+  this.forEach((element) => {
+    result.set(keySelector(element), element);
+  });
+  return result;
+};
+
+Array.prototype.groupBy = function <T, K, V>(
+  keySelector: (key: T) => K,
+  transform?: (value: T) => V
+) {
+  const result = new Map();
+  if (transform == undefined) {
+    for (let i = 0; i < this.length; i++) {
+      const group = result.get(keySelector(this[i]));
+      if (!group) {
+        result.set(keySelector(this[i]), [this[i]]);
+      } else {
+        group.push(this[i]);
+      }
     }
     return result;
-}
+  }
+  for (let i = 0; i < this.length; i++) {
+    const group = result.get(keySelector(this[i]));
+    if (!group) {
+      result.set(keySelector(this[i]), [transform(this[i])]);
+    } else {
+      group.push(transform(this[i]));
+    }
+  }
+  return result;
+};
 
-Array.prototype.chunked = function(chunkSize:number)
-{
-    let chunksCount = this.length/chunkSize;
-    if(this.length % chunkSize != 0)
-    {
-        chunksCount = Math.floor(chunksCount) + 1;
+Array.prototype.count = function <T>(predicate?: (value: T) => boolean) {
+  let count = 0;
+  if (predicate == undefined) {
+    return this.length;
+  }
+  this.forEach((element) => {
+    if (predicate(element)) {
+      count++;
     }
-    let result = [];
-    for(let i=0;i<this.length;i+=chunkSize)
-    {
-        const chunk = this.slice(i, i + chunkSize);
-        result.push(chunk)
-    }
-    return result;
-}
+  });
+  return count;
+};
 
-Array.prototype.average = function()
-{
-    let sum=0;
-    this.forEach(element=>
-    {
-        sum+=element;
-    });
-    return sum/this.length;
-}
+Array.prototype.flatten = function <T>() {
+  let result: T[] = [];
+  this.forEach((element) => {
+    if (Array.isArray(element)) {
+      result = result.concat(element.flatten());
+    } else {
+      result.push(element);
+    }
+  });
+  return result;
+};
 
-Array.prototype.filterIndexed = function<T>(predicate:(index:number,value:T)=>boolean)
-{
-    let result = [];
-    for(let i = 0;i<this.length;i++)
-    {
-        if(predicate(i,this[i]))
-        {
-            result.push(this[i]);
-        }
+Array.prototype.distinctBy = function <T>(selector: (key: T) => T) {
+  const result: T[] = [];
+  this.forEach((element) => {
+    if (selector(element)) {
+      const a = result.find((item) => selector(item) == selector(element));
+      if (!a) {
+        result.push(element);
+      }
     }
-    return result;
-}
-
-Array.prototype.filterNot = function<T>(predicate:(value:T)=>boolean)
-{
-    let result = [];
-    for(let i =0;i<this.length;i++)
-    {
-        if(!predicate(this[i]))
-        {
-            result.push(this[i]);
-        }
-    }
-    return result;
-}
-
-Array.prototype.maxBy = function<T>(selector:(type:T)=>number)
-{
-    let max = Number.MIN_VALUE;
-    let result = null;
-    for(let i = 0;i<this.length;i++)
-    {
-        if(selector(this[i])>max)
-        {
-            max = selector(this[i]);
-            result = this[i];
-        }
-    }
-    return result;
-}
-
-Array.prototype.minBy = function<T>(selector:(type:T)=>number)
-{
-    let min = Number.MAX_VALUE;
-    let result = null;
-    for(let i=0;i<this.length;i++)
-    {
-        if(selector(this[i])<min)
-        {
-            min = selector(this[i]);
-            result = this[i];
-        }
-    }
-    return result;
-}
-
-Array.prototype.fold = function(initialValue:string|number,operation:(value:string|number)=>string|number)
-{
-    let result:string|number = initialValue
-    for(let i =0;i<this.length;i++)
-    {
-        result += operation(this[i]) as string;
-    }
-    return result;
-}
-
-Array.prototype.associateBy = function<K,V>(keySelector:(key:K)=>V):Map<K,V>
-{
-    let result = new Map();
-    for(let i=0;i<this.length;i++)
-    {
-        result.set(keySelector(this[i]),this[i]);
-    }
-    return result;
-}
-
-Array.prototype.groupBy = function<T,K,V>(keySelector:(key:T)=>K,transform?:(value:T)=>V)
-{
-    let result = new Map();
-    if(transform == undefined)
-    {
-        for(let i =0;i<this.length;i++)
-        {
-            const group = result.get(keySelector(this[i]));
-            if(!group)
-            {   
-                result.set(keySelector(this[i]),[this[i]]);  
-            }
-            else
-            {
-                group.push(this[i]);
-            }
-        }
-        return result;
-    }
-    for(let i =0;i<this.length;i++)
-    {
-        const group = result.get(keySelector(this[i]));
-        if(!group)
-        {
-            result.set(keySelector(this[i]),[transform(this[i])]);  
-        }
-        else
-        {
-            group.push(transform(this[i]));
-        }
-    }
-    return result;
-}
-
-Array.prototype.count = function<T>(predicate?:(value:T)=>boolean)
-{
-    let count = 0;
-    if(predicate == undefined)
-    {
-        return this.length;
-    }
-    for(let i=0;i<this.length;i++)
-    {
-        if(predicate(this[i]))
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-Array.prototype.flatten = function<T>()
-{
-    let result:T[] = [];
-    for(var i = 0; i < this.length; i++) 
-    {
-        if(Array.isArray(this[i])) 
-        {
-            result = result.concat(this[i].flatten());
-        } else 
-        {
-            result.push(this[i]);
-        }
-    }
-    return result;
-}
-
-Array.prototype.distinctBy = function<T>(selector:(key:T)=>T)
-{
-    let result:T[]=[];
-    for(let i=0;i<this.length;i++)
-    {
-        if(selector(this[i]))
-        {
-            const a = result.find(item=>selector(item)==selector(this[i]));
-            if(!a)
-            {
-                result.push(this[i]);
-            }
-        }
-    }
-    return result;
-}
+  });
+  return result;
+};
--- ArrayСustom/src/app.ts
+++ ArrayСustom/src/app.ts
@@ -1,66 +1,70 @@
-import './ArrayExtentionMethods';
+import "./ArrayExtentionMethods";
 
-class Person
-{
-    id:number;
-    name:string;
-    code:number;
-    constructor(id:number,name:string,code:number)
-    {
-        this.id = id;
-        this.name = name;
-        this.code = code;
-    }
+class Person {
+  id: number;
+  name: string;
+  code: number;
+  constructor(id: number, name: string, code: number) {
+    this.id = id;
+    this.name = name;
+    this.code = code;
+  }
 }
 
-const persons:Person[]=[new Person(1,"Person1",100),
-new Person(2,"Person2",500),new Person(3,"Person3",300),new Person(4,"Person4",300)];
+const persons: Person[] = [
+  new Person(1, "Person1", 100),
+  new Person(2, "Person2", 500),
+  new Person(3, "Person3", 300),
+  new Person(4, "Person4", 300),
+];
 
-const stringsArray = ["string","someString","string1","string2"];
-const numbersArray = [1,2,3,4,5,6,7,8,9,10];
-const arrayOfArrays = [[1,2,3],[[4,5,[6,7]]],[8,9]];
-const emptyArray:string[] = [];
-const cahrs =['a','b','c']
+const stringsArray = ["string", "someString", "string1", "string2"];
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrayOfArrays = [[1, 2, 3], [[4, 5, [6, 7]]], [8, 9]];
+const emptyArray: string[] = [];
+const cahrs = ["a", "b", "c"];
 
 //Any
 console.log("Array.any() " + numbersArray.any());
 console.log("Array.any() " + emptyArray.any());
-console.log("Array.any(predicate) " + persons.any(item=>item.code == 300));
+console.log("Array.any(predicate) " + persons.any((item) => item.code == 300));
 //All
-console.log("Array.all(predicate) " + numbersArray.all(item=>item>0));
+console.log("Array.all(predicate) " + numbersArray.all((item) => item > 0));
 //FindLast
-console.log("Array.findLast(predicate) " + numbersArray.findLast(item=>item % 2== 0));
+console.log(
+  "Array.findLast(predicate) " + numbersArray.findLast((item) => item % 2 == 0)
+);
 //Chunked
 console.log(numbersArray.chunked(3));
 //Avedrage
-console.log("Array.average() "+numbersArray.average());
+console.log("Array.average() " + numbersArray.average());
 //Filter indexed
 console.log("Filter indexed: ");
-console.log(numbersArray.filterIndexed(index=>index % 2 != 0))
+console.log(numbersArray.filterIndexed((index) => index % 2 != 0));
 //FilterNot
 console.log("Filter not: ");
-console.log(persons.filterNot(item=>item.id == 1));
+console.log(persons.filterNot((item) => item.id == 1));
 //MaxBy & MinBy
 console.log("Max by:");
-console.log(persons.maxBy(item=>item.code));
-console.log("Min by:")
-console.log(stringsArray.minBy(item=>item.length));
+console.log(persons.maxBy((item) => item.code));
+console.log("Min by:");
+console.log(stringsArray.minBy((item) => item.length));
 //Fold
-console.log("Fold:")
-console.log(numbersArray.fold(100,item => item));
-console.log(stringsArray.fold("Strings: ",item=>` ${item}`));
+console.log("Fold:");
+console.log(numbersArray.fold(100, (item) => item));
+console.log(stringsArray.fold("Strings: ", (item) => ` ${item}`));
 //AssociateBy
 console.log("AssociateBy:");
-console.log(persons.associateBy(item=>item.code));
+console.log(persons.associateBy((item) => item.code));
 //GroupBy
 console.log("groupBy:");
-console.log(persons.groupBy(item=>item.code));
+console.log(persons.groupBy((item) => item.code));
 //Count
 console.log("Count: ");
-console.log(persons.count(item=>item.code==300));
+console.log(persons.count((item) => item.code == 300));
 //Flatten
-console.log("Flatten:")
+console.log("Flatten:");
 console.log(arrayOfArrays.flatten());
 //DistinctBy
 console.log("DistinctBy:");
-console.log(persons.distinctBy(item=>item.code));
+console.log(persons.distinctBy((item) => item.code));
